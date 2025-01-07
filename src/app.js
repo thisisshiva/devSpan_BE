df90c5ed548403308185555ded3f2693efd9017e
Shivashankar <shivashankarnatarajan97@gmail.com>
2025-01-07 21:27:52 +0530

    query&dynamic routes

--- src/app.js
+++ src/app.js
@@ -3,20 +3,30 @@ const express = require('express');
 const app = express();
 
 
-app.use("/host", (req, res) => {
-    console.log(req.url, req.method);
-    res.send('this is the host dashboard');
+app.get("/user", (req, res,next) => {
+    console.log(req.query);
+    res.send({name:'shiva',age: 21});
 });
 
-app.use("/user", (req, res) => {
-    console.log(req.url, req.method);
-    res.send('this is the user dashboard');
+app.get("/host/:userId/:name/:pswd", (req, res,next) => {
+    console.log(req.params);
+    res.send({name:'shiva',age: 21});
 });
 
-app.use("/", (req, res,next) => {
-    console.log(req.url, req.method);
-    res.send('this is the tinder msg');
-});
+// app.post("/host", (req, res) => {
+//     console.log(req.url, req.method);
+//     res.send('this is the host dashboard, Msg From Host');
+// });
+
+// app.get("/user", (req, res) => {
+//     console.log(req.url, req.method);
+//     res.send('this is the user dashboard');
+// });
+
+// app.use("/", (req, res,next) => {
+//     console.log(req.url, req.method);
+//     res.send('this is the tinder msg');
+// });
 
 const PORT = 7000;
 app.listen(PORT, () => {
