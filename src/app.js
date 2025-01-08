7f7b83438eb9b1ff09bce90dacd7419fcdb5005e
Shivashankar <shivashankarnatarajan97@gmail.com>
2025-01-08 13:51:49 +0530

    CURD API

--- src/app.js
+++ src/app.js
@@ -4,27 +4,99 @@ const User = require("./model/user");
 
 const app = express();
 
+app.use(express.json());
+
+//save new User Obj API
 app.post('/signup',async (req,res) => {
-
     //Creating New instance of the user model --OR-- in simple term creating a new User for the user_data
-    const user = new User({
-        firstName:'saru',
-        lastName:'Natarajan',
-        gender:'female',
-        email:'saru@gmail.com',
-        password:'saru@123'
-    })
-
+    const user = new User(req.body)
+    
     try{
-        await user.save();
+        const savedUser= await user.save();
         res.send("Data added successfully in db")
+        console.log(savedUser); 
     }
     catch(err){
         res.status(400).send('err occured while saving the data:'+err)
     }
-
 })
 
+//finding user by emailId Obj API
+app.get('/user', async (req,res) => {
+
+  try{
+    const user = await User.findOne({email : req.body.email}); 
+    if(!user){
+      res.status(400).send('Something went wrong')
+    }
+    else{
+      res.send(user);
+    }
+  }
+  catch(err){
+    res.status(400).send('Something went wrong')
+  }
+})
+
+//get all the user API
+app.get('/feed', async(req,res)=>{
+  // const allUser = req.body.email
+  try{
+    const allUser = await User.find({})
+    if(!allUser){
+      res.status(400).send('Something went wrong')
+    }
+    else{
+      res.send(allUser);
+      console.log(allUser);
+      
+    }
+  }
+  catch(err){
+    res.status(400).send('Something went wrong')
+  }
+})
+
+//delete user by Id Obj API
+app.delete('/deleteuser', async (req,res) => {
+
+  const userId = req.body.userId
+
+  try{
+    const delUser = await User.findByIdAndDelete(userId)
+    res.send(delUser)
+    console.log(delUser);
+    
+  }
+  catch(err){
+    console.log(err);
+    res.status(400).send('something went wrong')
+  }
+})
+
+//Update user by Id Obj API Using patch /patch update partially to update only required data not entire resourse 
+app.patch('/updateuser', async(req,res) => {
+
+  const userId = req.body.userId;
+  const data = req.body 
+
+  try{
+    const updatedUser = await User.findByIdAndUpdate({_id: userId},data)
+    if(!updatedUser){
+      res.status(400).send('err occured while updating user')
+    }
+    else{
+      res.send(updatedUser)
+      console.log(updatedUser);
+    }
+
+  }
+  catch(err){
+    console.log(err);
+    res.status(400).send('something went wrong')
+  }
+
+})
 
 dbConnect()
   .then(() => {
