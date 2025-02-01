b16a4d1d6753b07474bce0ad4f6a5dca216fee96
Shivashankar <shivashankarnatarajan97@gmail.com>
2025-02-01 17:10:41 +0530

    ft.frontend_url

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://devspan-frontend.onrender.com"],
     credentials: true,
   })
 );
