ac5ec2da992b7c449a16d3cf7df511063169b83b
Shivashankar <shivashankarnatarajan97@gmail.com>
2025-02-17 13:55:15 +0530

    fix: update CORS origin to allow requests from the new frontend URL

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["https://devspan-frontend-7pqf.onrender.com","http://localhost:5173"],
     credentials: true
   })
 );
